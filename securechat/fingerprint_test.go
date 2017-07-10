2441031ec85cd82ccffcb7e341b5ae320c41d723
Alexey Ermishkin <scratch.net@gmail.com>
2017-07-10 11:18:19 +0500

    added number fingerprint test

---
+++ securechat/fingerprint_test.go
@@ -0,0 +1,14 @@
+package securechat
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNumberFingerprint(t *testing.T) {
+	ids := []string{"b", "c", "a"}
+	fp, err := NumberFingerprint(ids)
+	assert.NoError(t, err)
+	assert.Equal(t, "95767 63932 18392 87777 58010 79361 43185 89666 69268 33576 75875 36436", fp)
+}
