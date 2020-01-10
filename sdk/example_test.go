4d0a921c1b3e070fd46eafcdd68be4577cc9c96e
Vladimir Vinogradov <vinogradovve@gmail.com>
2020-01-10 17:50:27 +0300

    fix

--- sdk/example_test.go
+++ sdk/example_test.go
@@ -13,13 +13,41 @@ import (
 	"github.com/VirgilSecurity/virgil-sdk-go/storage"
 )
 
-func ExampleCreateCard() {
-	card := registerCard("Alice")
+func ExampleCardManager_PublishCard() {
+	const identity = "Alice"
+	var crypto crypto.Crypto
+
+	// generate a key pair
+	keypair, err := crypto.GenerateKeypair()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	privateKeyStorage := storage.NewVirgilPrivateKeyStorage(&storage.FileStorage{})
+
+	// clean up storage before write
+	privateKeyStorage.Delete(identity)
+
+	// save a private key into key storage
+	err = privateKeyStorage.Store(keypair, identity, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cardManager := setupCardManager()
+	// publish user's on the Cards Service
+	card, err := cardManager.PublishCard(&sdk.CardParams{
+		PrivateKey: keypair,
+		Identity:   identity,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(card.Identity)
 	// Output: Alice
 }
 
-func ExampleEncryptDecrypt() {
+func Example_encryptDecrypt() {
 	const (
 		alice = "Alice"
 		bob   = "Bob"
@@ -30,6 +58,7 @@ func ExampleEncryptDecrypt() {
 	registerCard(bob)
 	encryptedMsg := encrypt(alice, bob, []byte(text))
 	decryptedMsg := decrypt(alice, bob, encryptedMsg)
+	fmt.Printf("len(origin): %v len(encrypt): %v len(decrypt): %v\n", len(text), len(encryptedMsg), len(decryptedMsg))
 
 	fmt.Printf("%s\n", decryptedMsg)
 	// Output: Hello, Bob!
@@ -53,6 +82,7 @@ func encrypt(from string, to string, messageToEncrypt []byte) []byte {
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Printf("cards: %v\n", len(cards))
 
 	// sign a message with a private key then encrypt using Bob's public keys
 	encryptedMessage, err := crypto.SignThenEncrypt(messageToEncrypt, fromPrivateKey, cards.ExtractPublicKeys()...)
