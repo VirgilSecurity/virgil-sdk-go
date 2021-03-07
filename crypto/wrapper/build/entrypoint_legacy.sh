96d70d318d5b1e113da8a675506c09a124803595
rbobrovnikov <rbobrovnikov@virgilsecurity.com>
2021-03-07 06:10:48 +0000

    +epel, pip3 install

--- crypto/wrapper/build/entrypoint_legacy.sh
+++ crypto/wrapper/build/entrypoint_legacy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source scl_source enable devtoolset-2
-source scl_source enable rh-python35
-pip install protobuf
+# Python35 installed from sources and absent in SCL
+#source scl_source enable rh-python35
+pip3 install protobuf
 exec "$@"
